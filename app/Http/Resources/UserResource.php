813149c56b8737b4a0cb46ab3b4515c641ba304f
alessamashitah <alessa.mashitahzj@gmail.com>
2022-03-11 23:47:22 +0800

    remove comment

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,12 +14,6 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        // return parent::toArray($request);
-        // return [
-        //     'name' => $this->name,
-        //     'email' => $this->email, 
-        // ];
-
         return [
             'data' => [
                 'user' => [
