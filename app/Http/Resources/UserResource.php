d94a20e24c6b47b37010255b8cf676b395da93a1
alessamashitah <alessa.mashitahzj@gmail.com>
2022-03-11 17:36:47 +0800

    add User Resources

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+        // return [
+        //     'name' => $this->name,
+        //     'email' => $this->email, 
+        // ];
+
+        return [
+            'data' => [
+                'user' => [
+                    'name'  => $this->name,
+                    'email' => $this->email,
+                ]
+            ]
+        ];
+    }
+
+    public function with($request)
+    {
+        return [
+            'success' => true,
+            'message' => 'successful show user',
+        ];
+    }
+}
+
