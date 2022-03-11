e1e37d877d0e049b26586b559a9577532df4b7e2
alessamashitah <alessa.mashitahzj@gmail.com>
2022-03-11 16:40:18 +0800

    add validation of inputs

---
+++ app/Http/Requests/UserStoreRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'username' => 'required|min:5|max:12',
+            'state' => 'required|max:50',
+            'district' => 'required|max:50'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Name is required!',
+            'email.required' => 'Email is required!',
+            'password.required' => 'Password is required!',
+            'username.required' => 'Username is required!',
+            'state.required' => 'State is required!',
+            'district.required' => 'District is required!'
+        ];
+    }
+}
