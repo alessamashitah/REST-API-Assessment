866937014e0f5c296185815c267b975c879fa31a
alessamashitah <alessa.mashitahzj@gmail.com>
2022-03-11 16:38:17 +0800

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserAPIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +19,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->get('/fetch/users', [App\Http\Controllers\API\UserAPIController::class, 'index']);
+// Route::middleware('auth:api')->get('/fetch/users', [App\Http\Controllers\API\UserAPIController::class, 'index']);
+
+Route::middleware('auth:api')->prefix('v1')->group(function(){
+
+    Route::get('/users', [UserAPIController::class,'index']);
+    Route::get('/users/show/{user}',[UserAPIController::class,'show']);
+    Route::post('/users/create', [UserAPIController::class,'store']);
+    Route::post('/users/update/{user}',[UserAPIController::class,'update']);
+    Route::delete('/users/{user}',[UserAPIController::class,'destroy']);
+
+}); 
